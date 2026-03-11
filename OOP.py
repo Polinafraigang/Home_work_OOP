56cc6e7f39a5522f876b6a734fe65da4b4a561de
Polina Freigang <fraigang29@gmail.com>
2026-03-11 13:28:25 +0500

    Zadanie 3

--- OOP.py
+++ OOP.py
@@ -1,6 +1,7 @@
 import self
+from functools import total_ordering
 
-
+@total_ordering
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -20,19 +21,58 @@ class Student:
         else:
             return 'Ошибка'
 
+        def _calc_average(self):
+            total_grade = 0
+            count_grade = 0
+            for subbject_grade in self.grades.values():
+                total_grade += sum(subbject_grade)
+                count_grade += len(subbject_grade)
+            return 0 if count_grade == 0 else total_grade / count_grade
+
+        def __str__(self):
+            return f'''Имя: {self.name}
+    Фамилия: {self.surname}
+    Средняя оценка за домашние задания: {self._calc_average()}
+    Курсы в процессе изучения: {", ".join(self.courses_in_progress)}
+    Завершенные курсы: {", ".join(self.finished_courses)}'''
+
+    def __eq__(self, other):
+        if not isinstance(other, Student):
+            return NotImplemented
+        return self._calc_average() == other._calc_average()
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            return NotImplemented
+        return self._calc_average() < other._calc_average()
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+    def __str__(self):
+        return f'''Имя: {self.name}
+Фамилия: {self.surname}'''
 
-
+@total_ordering
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
 
+    def __str__(self):
+        return super().__str__() + f'\nСредняя оценка за лекции: {self._calc_average()}'
+
+    def __eq__(self, other):
+        if not isinstance(other, Lecturer):
+            return NotImplemented
+        return self._calc_average() == other._calc_average()
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            return NotImplemented
+        return self._calc_average() < other._calc_average()
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -44,18 +84,3 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
-
-lecturer = Lecturer('Иван', 'Иванов')
-reviewer = Reviewer('Пётр', 'Петров')
-student = Student('Алёхина', 'Ольга', 'Ж')
-
-student.courses_in_progress += ['Python', 'Java']
-lecturer.courses_attached += ['Python', 'C++']
-reviewer.courses_attached += ['Python', 'C++']
-
-print(student.rate_lecture(lecturer, 'Python', 7))  # None
-print(student.rate_lecture(lecturer, 'Java', 8))  # Ошибка
-print(student.rate_lecture(lecturer, 'С++', 8))  # Ошибка
-print(student.rate_lecture(reviewer, 'Python', 6))  # Ошибка
-
-print(lecturer.grades)  # {'Python': [7]}
